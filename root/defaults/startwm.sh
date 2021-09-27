a1baba455a088138ac1bed811b0b7509af18b213
thelamer <ryankuba@gmail.com>
2021-09-27 11:10:12 -0700

    updating arch kde to new baseimage format with pulse script

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /usr/bin/pulseaudio --start
+PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh --start &
 /usr/bin/startplasma-x11 > /dev/null 2>&1
