ca8c35c89143a409f6b59714494fead10ff0c493
ryan.kuba <ryan.kuba@kasmweb.com>
2023-03-19 11:22:38 -0700

    explicity disable compositing on init

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -3,4 +3,9 @@
 setterm blank 0
 setterm powerdown 0
 xset s 0 0
+if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  sed -i \
+    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
+    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+fi
 /usr/bin/xfce4-session > /dev/null 2>&1
