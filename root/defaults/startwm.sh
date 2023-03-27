1013cc7caf0d8600ce9a22f7a3bc11f46b08e07d
thelamer <ryankuba@gmail.com>
2023-03-27 12:27:55 -0700

    point readme to master screen blanking tweeks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ ! -f /config/.config/kwinrc ]; then
-echo '[Compositing]
-Enabled=false' > /config/.config/kwinrc
+if [ ! -f $HOME/.config/kwinrc ]; then
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
 fi
 setterm blank 0
 setterm powerdown 0
-xset s 0 0
 /usr/bin/dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
