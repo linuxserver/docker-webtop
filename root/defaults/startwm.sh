85371c83a5571dfa16830fcc59761411e8eebe82
thelamer <ryankuba@gmail.com>
2023-03-27 13:29:47 -0700

    point readme to master screen blanking tweeks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [ ! -f /config/.config/kwinrc ]; then
-echo '[Compositing]
-Enabled=false' > /config/.config/kwinrc
+if [ ! -f $HOME/.config/kwinrc ]; then
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+fi
+if [ ! -f $HOME/.config/kdeglobals ]; then
+  kwriteconfig5 --file $HOME/.config/kdeglobals --group KDE --key LookAndFeelPackage org.fedoraproject.fedora.desktop
 fi
 setterm blank 0
 setterm powerdown 0
-xset s 0 0
 /usr/bin/startplasma-x11 > /dev/null 2>&1
