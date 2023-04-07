e72c17bdaa62f66e2231c1697235aaf0b3eb8e98
Ryan Kuba <ryankuba@gmail.com>
2023-04-07 23:58:15 +0000

    rebase ubuntu-xfce to kasmvnc image (#136)
    * rebase ubuntu-xfce to kasmvnc image
    
    * point readme to master screen blanking tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-/startpulse.sh &
-/usr/bin/startxfce4 > /dev/null 2>&1
+
+setterm blank 0
+setterm powerdown 0
+if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  sed -i \
+    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
+    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+fi
+/usr/bin/xfce4-session > /dev/null 2>&1
