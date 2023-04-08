ac9ef31e709f5bd3d1ac1bc11349de81d687a69c
Ryan Kuba <ryankuba@gmail.com>
2023-04-08 01:38:50 +0000

    Rebase alpine kde (#116)
    * rebase alpine-kde to new kasmvnc baseimage
    
    * use correct arch
    
    * use os versioning
    
    * update branch readmes to point to master
    
    * cleanup screen blanking settings
    
    * finalize screen blanking
    
    * update bin wrapper to pass cli options
    
    ---------
    
    Co-authored-by: ryan.kuba <ryan.kuba@kasmweb.com>
    Co-authored-by: thelamer <ryankuba@gmai.com>

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-/startpulse.sh &
+
+if [ ! -f $HOME/.config/kwinrc ]; then
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+fi
+setterm blank 0
+setterm powerdown 0
 /usr/bin/startplasma-x11 > /dev/null 2>&1
