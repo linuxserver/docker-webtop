cf21bbdd9c20f4991bb4259eab4f25b30accf10c
Ryan Kuba <ryankuba@gmail.com>
2023-04-08 00:50:47 +0000

    Rebase arch xfce (#124)
    * rebase arch-xfce to new kasmvnc baseimage
    
    * swap to os for tagging
    
    * point readme to master screen blanking tweeks
    
    * update bin wrapper to pass cli options
    
    ---------
    
    Co-authored-by: ryan.kuba <ryan.kuba@kasmweb.com>
    Co-authored-by: thelamer <ryankuba@gmai.com>

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh --start &
-/usr/bin/startxfce4 > /dev/null 2>&1
+
+setterm blank 0
+setterm powerdown 0
+if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  sed -i \
+    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
+    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+fi
+/usr/bin/dbus-launch /usr/bin/startxfce4 --replace > /dev/null 2>&1
