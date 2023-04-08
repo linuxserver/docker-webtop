d6ad154a4808c14fa7e70e943d8350ed210166db
Ryan Kuba <ryankuba@gmail.com>
2023-04-08 00:42:44 +0000

    Rebase fedora kde (#127)
    * rebase fedora-kde to new kasmvnc baseimage
    
    * remove debug
    
    * point readme to master screen blanking tweeks
    
    * update bin wrapper to pass cli options
    
    ---------
    
    Co-authored-by: ryan.kuba <ryan.kuba@kasmweb.com>
    Co-authored-by: thelamer <ryankuba@gmai.com>

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-/startpulse.sh &
+
+if [ ! -f $HOME/.config/kwinrc ]; then
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+fi
+if [ ! -f $HOME/.config/kdeglobals ]; then
+  kwriteconfig5 --file $HOME/.config/kdeglobals --group KDE --key LookAndFeelPackage org.fedoraproject.fedora.desktop
+fi
+setterm blank 0
+setterm powerdown 0
 /usr/bin/startplasma-x11 > /dev/null 2>&1
