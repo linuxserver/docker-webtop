44a52cb6eca5b71cbf0b0eef9d4b8b26ab47698c
Ryan Kuba <ryankuba@gmail.com>
2023-04-08 01:20:15 +0000

    rebase arch-kde to new kasmvnc baseimage (#121)
    * rebase arch-kde to new kasmvnc baseimage
    
    * point readme to master screen blanking tweeks
    
    * update bin wrapper to pass cli options
    
    ---------
    
    Co-authored-by: thelamer <ryankuba@gmai.com>

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-PULSE_SCRIPT=/etc/xrdp/pulse/default.pa /startpulse.sh --start &
-/usr/bin/startplasma-x11 > /dev/null 2>&1
+
+if [ ! -f $HOME/.config/kwinrc ]; then
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+fi
+if [ ! -f $HOME/.config/kscreenlockerrc ]; then
+  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+fi
+setterm blank 0
+setterm powerdown 0
+/usr/bin/dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
