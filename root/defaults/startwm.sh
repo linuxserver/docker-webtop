d4a85758056ab8fd4f7e5aefd41d915108fa60f8
Ryan Kuba <ryankuba@gmail.com>
2023-04-08 01:59:47 +0000

    Update alpine-xfce to new KasmVNC baseimage, update docs (#113)
    * Update alpine-xfce to new KasmVNC baseimage, update docs
    
    * finalize screen blanking
    
    * update bin wrapper to pass cli options
    
    ---------
    
    Co-authored-by: thelamer <ryankuba@gmai.com>

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-/startpulse.sh &
-/usr/bin/startxfce4 > /dev/null 2>&1
+
+if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  sed -i \
+    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
+    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+fi
+/usr/bin/xfce4-session > /dev/null 2>&1
