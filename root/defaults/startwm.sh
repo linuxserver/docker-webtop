13428ca13e26052afdaed076333dc0b6918b213f
Ryan Kuba <ryankuba@gmail.com>
2024-04-22 12:11:58 -0700

    Noble xfce (#237)
    * rebase xfce to noble
    
    * xfce tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -2,9 +2,16 @@
 
 setterm blank 0
 setterm powerdown 0
-if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
-  sed -i \
-    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
-    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+if [ ! -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
+  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
+cat <<EOT >> "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<channel name="xfwm4" version="1.0">
+  <property name="general" type="empty">
+    <property name="use_compositing" type="bool" value="false"/>
+  </property>
+</channel>
+EOT
 fi
+
 /usr/bin/xfce4-session > /dev/null 2>&1
