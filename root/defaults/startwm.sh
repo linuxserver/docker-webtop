0796e26f54daf533a2954efb57f80889fd7b5aca
thelamer <ryankuba@gmail.com>
2024-05-23 15:48:13 -0400

    nvidia compat

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Disable compositing and screen lock
 if [ ! -f $HOME/.config/kwinrc ]; then
   kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
 fi
@@ -8,4 +16,6 @@ if [ ! -f $HOME/.config/kscreenlockerrc ]; then
 fi
 setterm blank 0
 setterm powerdown 0
+
+# Launch DE
 /usr/bin/startplasma-x11 > /dev/null 2>&1
