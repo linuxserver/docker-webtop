7375e9a163229de8dc25803949cb99db5fdd6dd5
thelamer <ryankuba@gmail.com>
2024-05-23 15:53:55 -0400

    nvidia compat

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Disable compositing
 setterm blank 0
 setterm powerdown 0
 gsettings set org.mate.Marco.general compositing-manager false
+
+# Launch DE
 /usr/bin/mate-session > /dev/null 2>&1
