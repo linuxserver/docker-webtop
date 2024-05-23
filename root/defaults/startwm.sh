8505f9d581197cd3d4c767c547d95253903a0f36
thelamer <ryankuba@gmail.com>
2024-05-23 16:07:14 -0400

    nvidia compat

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Launch DE
 setterm blank 0
 setterm powerdown 0
 /usr/bin/openbox-session > /dev/null 2>&1
