bbcccea4f0e2ca9aa2df9e39f07a309e5c7588c3
thelamer <ryankuba@gmail.com>
2024-05-23 08:45:05 -0400

    pull out env vars on alpine

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Disable compositing
 if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
   sed -i \
     '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
     "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 fi
+
+# Start DE
 /usr/bin/xfce4-session > /dev/null 2>&1
