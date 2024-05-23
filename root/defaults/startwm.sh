24383ad757b759bd2395182d80d96ed096da40da
thelamer <ryankuba@gmail.com>
2024-05-23 15:46:18 -0400

    nvidia compat

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Disable compositing
 setterm blank 0
 setterm powerdown 0
 if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
@@ -7,4 +16,6 @@ if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
     '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
     "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 fi
+
+# Launch DE
 /usr/bin/xfce4-session > /dev/null 2>&1
