82f11d4c59281a06f942074a8fa7e147af392509
thelamer <ryankuba@gmail.com>
2024-05-23 11:54:08 -0400

    use proper kde6 bin and nvidia support

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Disable compositing and screen locking
 if [ ! -f $HOME/.config/kwinrc ]; then
-  kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
+  kwriteconfig6 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
 fi
 if [ ! -f $HOME/.config/kscreenlockerrc ]; then
-  kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
+  kwriteconfig6 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
 fi
 if [ ! -f $HOME/.config/kdeglobals ]; then
-  kwriteconfig5 --file $HOME/.config/kdeglobals --group KDE --key LookAndFeelPackage org.fedoraproject.fedora.desktop
+  kwriteconfig6 --file $HOME/.config/kdeglobals --group KDE --key LookAndFeelPackage org.fedoraproject.fedora.desktop
 fi
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
 setterm blank 0
 setterm powerdown 0
 /usr/bin/startplasma-x11 > /dev/null 2>&1
