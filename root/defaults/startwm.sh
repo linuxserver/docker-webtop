4da51ed1c1dab215d011056dfec4af3e0db1aa0b
thelamer <ryankuba@gmail.com>
2025-06-16 16:49:47 -0400

    rebase fedora-xfce to selkies setup default settings

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
 
 # Enable Nvidia GPU support if detected
 if which nvidia-smi; then
@@ -7,14 +7,11 @@ if which nvidia-smi; then
   export GALLIUM_DRIVER=zink
 fi
 
-# Disable compositing
-setterm blank 0
-setterm powerdown 0
-if [ -f "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml ]; then
-  sed -i \
-    '/use_compositing/c <property name="use_compositing" type="bool" value="false"/>' \
-    "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
+# Default settings
+if [ ! -d "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
+  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml
+  cp /defaults/xfce/* "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
 fi
 
-# Launch DE
-/usr/bin/xfce4-session > /dev/null 2>&1
+# Start DE
+dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
