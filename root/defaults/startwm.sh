173ffdb9ec49ea2d018c473216c599c8f1eebf0f
Ryan Kuba <ryankuba@gmail.com>
2025-06-17 14:52:29 +0000

    rebase ubuntu-kde to selkies setup default settings (#316)

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Enable Nvidia GPU support if detected
-if which nvidia-smi; then
-  export LIBGL_KOPPER_DRI2=1
-  export MESA_LOADER_DRIVER_OVERRIDE=zink
-  export GALLIUM_DRIVER=zink
-fi
-
 # Disable compositing and screen lock
 if [ ! -f $HOME/.config/kwinrc ]; then
   kwriteconfig5 --file $HOME/.config/kwinrc --group Compositing --key Enabled false
@@ -14,8 +7,55 @@ fi
 if [ ! -f $HOME/.config/kscreenlockerrc ]; then
   kwriteconfig5 --file $HOME/.config/kscreenlockerrc --group Daemon --key Autolock false
 fi
+
+# Power related
 setterm blank 0
 setterm powerdown 0
 
-# Launch DE
-/usr/bin/dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
+# Direcotries
+sudo rm -f /usr/share/dbus-1/system-services/org.freedesktop.UDisks2.service
+mkdir -p "${HOME}/.config/autostart" "${HOME}/.XDG" "${HOME}/.local/share/"
+chmod 700 "${HOME}/.XDG"
+touch "${HOME}/.local/share/user-places.xbel"
+
+# Background perm loop
+if [ ! -d $HOME/.config/kde.org ]; then
+  (
+    loop_end_time=$((SECONDS + 30))
+    while [ $SECONDS -lt $loop_end_time ]; do
+        find "$HOME/.cache" "$HOME/.config" "$HOME/.local" -type f -perm 000 -exec chmod 644 {} + 2>/dev/null
+        sleep .1
+    done
+  ) &
+fi
+
+# Create startup script if it does not exist (keep in sync with openbox)
+STARTUP_FILE="${HOME}/.config/autostart/autostart.desktop"
+if [ ! -f "${STARTUP_FILE}" ]; then
+  echo "[Desktop Entry]" > $STARTUP_FILE
+  echo "Exec=bash /config/.config/openbox/autostart" >> $STARTUP_FILE
+  echo "Icon=dialog-scripts" >> $STARTUP_FILE
+  echo "Name=autostart" >> $STARTUP_FILE
+  echo "Path=" >> $STARTUP_FILE
+  echo "Type=Application" >> $STARTUP_FILE
+  echo "X-KDE-AutostartScript=true" >> $STARTUP_FILE
+  chmod +x $STARTUP_FILE
+fi
+
+# Check for any render nodes
+for node in /dev/dri/renderD*; do
+    if [ -e "$node" ]; then
+        render_node_exists=true
+        break
+    fi
+done
+# Enable Zink support if detected
+if [ "$render_node_exists" = true ] && [ "${DISABLE_ZINK}" == "false" ]; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Stat DE
+unset LD_PRELOAD
+dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
