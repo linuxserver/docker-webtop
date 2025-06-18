c4c87f882b0bb3973340a6b079284c6819a815df
thelamer <ryankuba@gmail.com>
2025-06-18 09:15:17 -0400

    use nvidia-smi to gate zink

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -42,19 +42,11 @@ if [ ! -f "${STARTUP_FILE}" ]; then
   chmod +x $STARTUP_FILE
 fi
 
-# Check for any render nodes
-for node in /dev/dri/renderD*; do
-    if [ -e "$node" ]; then
-        render_node_exists=true
-        break
-    fi
-done
-# Enable Zink support if detected
-if [ "$render_node_exists" = true ] && [ "${DISABLE_ZINK}" == "false" ]; then
+# Enable Nvidia GPU support if detected
+if which nvidia-smi && [ "${DISABLE_ZINK}" == "false" ]; then
   export LIBGL_KOPPER_DRI2=1
   export MESA_LOADER_DRIVER_OVERRIDE=zink
   export GALLIUM_DRIVER=zink
-  unset LD_PRELOAD
 fi
 
 # Start DE
