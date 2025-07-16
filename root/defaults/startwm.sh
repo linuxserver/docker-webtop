6634cc7fc77d45d74fdee4efc404da9d68448e43
thelamer <ryankuba@gmail.com>
2025-07-16 18:02:45 -0400

    wrap dbus

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -7,7 +7,13 @@ if which nvidia-smi; then
   export GALLIUM_DRIVER=zink
 fi
 
-# Launch DE
 setterm blank 0
 setterm powerdown 0
-/usr/bin/i3 > /dev/null 2>&1
+
+# Dbus defaults
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+mkdir -p -m700 "${XDG_RUNTIME_DIR}"
+chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
+
+# Start DE
+exec dbus-launch --exit-with-session /usr/bin/i3 > /dev/null 2>&1
