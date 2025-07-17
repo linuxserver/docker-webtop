929bcb72e0379b4cd8f33f090db31fc4820492bc
thelamer <ryankuba@gmail.com>
2025-07-17 09:58:12 -0400

    tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -8,9 +8,8 @@ fi
 
 
 # Dbus defaults
-export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
 mkdir -p -m700 "${XDG_RUNTIME_DIR}"
-chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
 
 # Start DE
 exec dbus-launch --exit-with-session /usr/bin/xfce4-session > /dev/null 2>&1
