71d751b853de2c5dc0d0eb632cfea5ef2ada4275
thelamer <ryankuba@gmail.com>
2025-07-17 09:59:11 -0400

    tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -5,9 +5,8 @@ setterm blank 0
 setterm powerdown 0
 
 # Dbus defaults
-export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
 mkdir -p -m700 "${XDG_RUNTIME_DIR}"
-chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
 
 # Start DE
 exec dbus-launch --exit-with-session /usr/bin/mate-session > /dev/null 2>&1
