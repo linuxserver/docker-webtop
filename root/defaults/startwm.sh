b0dd5f0c27be3a528e566d5cca9f1223f1f1b3d1
thelamer <ryankuba@gmail.com>
2025-07-17 10:32:27 -0400

    wrap dbus

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -13,5 +13,9 @@ if [ ! -d "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
   cp /defaults/xfce/* "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
 fi
 
+# Dbus defaults
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
+mkdir -p -m700 "${XDG_RUNTIME_DIR}"
+
 # Start DE
 exec dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
