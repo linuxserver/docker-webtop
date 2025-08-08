9c58c4d6f35e9339f408a80454e5b5b1098be6f8
thelamer <ryankuba@gmail.com>
2025-08-08 09:26:46 -0400

    update nvidia detection to handle hosts that use runtime with no GPU

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable Nvidia GPU support if detected
-if which nvidia-smi && [ "${DISABLE_ZINK}" == "false" ]; then
+if which nvidia-smi > /dev/null 2>&1 && ls -A /dev/dri 2>/dev/null && [ "${DISABLE_ZINK}" == "false" ]; then
   export LIBGL_KOPPER_DRI2=1
   export MESA_LOADER_DRIVER_OVERRIDE=zink
   export GALLIUM_DRIVER=zink
