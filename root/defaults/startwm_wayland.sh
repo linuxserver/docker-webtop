6b357a5c4f4943b5d716190e1f23b01898b07ebb
thelamer <ryankuba@gmail.com>
2025-12-27 09:30:01 -0500

    null route xfce logs

--- root/defaults/startwm_wayland.sh
+++ root/defaults/startwm_wayland.sh
@@ -9,4 +9,4 @@ fi
 # Start DE
 WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
 sleep 2
-exec dbus-launch --exit-with-session /usr/bin/xfce4-session
+exec dbus-launch --exit-with-session /usr/bin/xfce4-session > /dev/null 2>&1
