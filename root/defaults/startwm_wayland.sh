d6a2f4c9acbbcbce8af4e8a2383d79f5d46fc6fb
thelamer <ryankuba@gmail.com>
2025-12-28 09:14:38 -0500

    add wayland init logic

---
+++ root/defaults/startwm_wayland.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+setterm blank 0
+setterm powerdown 0
+
+# Start DE
+WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
+sleep 2
+exec dbus-launch --exit-with-session /usr/bin/i3 > /dev/null 2>&1
