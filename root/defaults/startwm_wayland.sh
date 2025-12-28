95d56d6b075321567aa6060b179027cf4d33d5ee
thelamer <ryankuba@gmail.com>
2025-12-28 09:24:51 -0500

    add wayland init logic

---
+++ root/defaults/startwm_wayland.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/with-contenv bash
+
+# Default settings
+if [ ! -d "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
+  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml
+  cp /defaults/xfce/* "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+
+# Start DE
+WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
+sleep 2
+dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
