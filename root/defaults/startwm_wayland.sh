c9377620525d44374867d22cab07a7fb6e272147
thelamer <ryankuba@gmail.com>
2025-12-28 08:52:10 -0500

    add wayland init logic

---
+++ root/defaults/startwm_wayland.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi > /dev/null 2>&1 && ls -A /dev/dri 2>/dev/null && [ "${DISABLE_ZINK}" == "false" ]; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Launch DE
+setterm blank 0
+setterm powerdown 0
+WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
+sleep 2
+/usr/bin/i3 > /dev/null 2>&1
