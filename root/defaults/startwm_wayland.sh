4b01630e8c2785b97850bd263a901a8cc0778c38
thelamer <ryankuba@gmail.com>
2025-12-28 08:48:41 -0500

    add wayland init logic

---
+++ root/defaults/startwm_wayland.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bash
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi > /dev/null 2>&1 && ls -A /dev/dri 2>/dev/null && [ "${DISABLE_ZINK}" == "false" ]; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Default settings
+if [ ! -d "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
+  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml
+  cp /defaults/xfce/* "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+
+# Start DE
+WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
+sleep 2
+exec dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
