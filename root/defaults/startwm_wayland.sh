d982ad92acfc6f2dceee3b08b597cfb9804e6855
thelamer <ryankuba@gmail.com>
2025-12-28 09:29:55 -0500

    pull zink export wayland

--- root/defaults/startwm_wayland.sh
+++ root/defaults/startwm_wayland.sh
@@ -42,13 +42,6 @@ if [ ! -f "${STARTUP_FILE}" ]; then
   chmod +x $STARTUP_FILE
 fi
 
-# Enable Nvidia GPU support if detected
-if which nvidia-smi > /dev/null 2>&1 && ls -A /dev/dri 2>/dev/null && [ "${DISABLE_ZINK}" == "false" ]; then
-  export LIBGL_KOPPER_DRI2=1
-  export MESA_LOADER_DRIVER_OVERRIDE=zink
-  export GALLIUM_DRIVER=zink
-fi
-
 # Start DE
 WAYLAND_DISPLAY=wayland-1 Xwayland :1 &
 sleep 2
