cb893e618083e51df4cf0922ce3cb495d596756a
thelamer <ryankuba@gmail.com>
2026-07-20 14:11:27 -0400

    purge labwc overrides in wayland mode xfce

--- root/defaults/startwm_wayland.sh
+++ root/defaults/startwm_wayland.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Default settings
-if [ ! -d "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml ]; then
-  mkdir -p "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml
-  cp /defaults/xfce/* "${HOME}"/.config/xfce4/xfconf/xfce-perchannel-xml/
-fi
+# Purge default labwc config
+rm -Rf $HOME/.config/labwc
 
 # Start DE
 WAYLAND_DISPLAY=wayland-1 startxfce4 --wayland > /dev/null 2>&1
